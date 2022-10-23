2faa7f1298902aacc1015354850690766f91424a
Chris Norman <chris.norman2@googlemail.com>
2022-10-23 18:57:26 +0100

    Remove call to `toList`.

--- lib/src/map_level.dart
+++ lib/src/map_level.dart
@@ -67,7 +67,6 @@ class MapLevel extends Level {
                     position: item.coordinates?.toDouble(),
                   ),
                 )
-                .toList()
           ],
         );
 
