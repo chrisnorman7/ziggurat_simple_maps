22e30cd9a70ab10696fc3a783f020d51d17eb7ed
Chris Norman <chris.norman2@googlemail.com>
2022-11-07 19:47:15 +0000

    Export the `RumbleEffect` class.

--- lib/ziggurat_simple_maps.dart
+++ lib/ziggurat_simple_maps.dart
@@ -4,4 +4,5 @@ library ziggurat_simple_maps;
 export 'src/map_level.dart';
 export 'src/map_level_item.dart';
 export 'src/map_level_terrain.dart';
+export 'src/rumble_effect.dart';
 export 'src/watch_item.dart';
